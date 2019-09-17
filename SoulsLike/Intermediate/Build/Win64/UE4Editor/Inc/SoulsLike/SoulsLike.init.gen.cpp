1bd3edeb3a39ef1a94681609d9a75570a7ed6957
Ella <ellazhang37@gmail.com>
2019-09-17 12:41:41 -0400

    * Fix logic of roll dodge and attack

--- SoulsLike/Intermediate/Build/Win64/UE4Editor/Inc/SoulsLike/SoulsLike.init.gen.cpp
+++ SoulsLike/Intermediate/Build/Win64/UE4Editor/Inc/SoulsLike/SoulsLike.init.gen.cpp
@@ -21,8 +21,8 @@ void EmptyLinkFunctionForGeneratedCodeSoulsLike_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0xE0211780,
-				0xBC5DB04B,
+				0xD3605AE2,
+				0x9FAC7B7C,
 				METADATA_PARAMS(nullptr, 0)
 			};
 			UE4CodeGen_Private::ConstructUPackage(ReturnPackage, PackageParams);
