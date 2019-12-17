046460208c6b5902db232397e5d44e049c4ed4ad
Eddie <xiaoyefm@live.cn>
2019-12-17 15:24:33 -0500

    Bo_Mechanic 2

--- SoulsLike/Source/SoulsLike/Bo_Mechanic.cpp
+++ SoulsLike/Source/SoulsLike/Bo_Mechanic.cpp
@@ -4,7 +4,10 @@
 #include "Bo_Mechanic.h"
 #include "Engine/World.h"
 #include "Components/SphereComponent.h"
+#include "Components/PointLightComponent.h"
 #include "TimerManager.h"
+#include "Engine.h"
+
 
 // Sets default values
 ABo_Mechanic::ABo_Mechanic()
@@ -31,14 +34,21 @@ ABo_Mechanic::ABo_Mechanic()
 	MySceneFifth= CreateDefaultSubobject<USceneComponent>(TEXT("My Mesh5"));
 	MySceneFifth->SetupAttachment(RootComponent);
 
+	LightIntensity = 8000.f;
+	PointLight = CreateDefaultSubobject<UPointLightComponent>(TEXT("Point Light"));
+	PointLight->Intensity = LightIntensity;
+	PointLight->bVisible = true;
+	RootComponent = PointLight;
+
 
 }
+
 
 // Called when the game starts or when spawned
 void ABo_Mechanic::BeginPlay()
 {
 	Super::BeginPlay();
-	GetWorldTimerManager().SetTimer(MemberTimerHandle, this, &ABo_Mechanic::BallAttack, Delay1, true, Delay2);
+	GetWorldTimerManager().SetTimer(MemberTimerHandle, this, &ABo_Mechanic::GetRadomDelay, Delay1, true, 1);
 }
 
 // Called every frame
@@ -49,38 +59,54 @@ void ABo_Mechanic::Tick(float DeltaTime)
 	
 }
 
-void ABo_Mechanic::BallAttack()
+void ABo_Mechanic::GetRadomDelay()
 {
 	FMath random;
-	float temp = random.RandRange(0, 4);
+	Delay2 = random.RandRange(2, 6);
+	PointLight->ToggleVisibility();
+	GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Yellow, FString::Printf(TEXT("number:%d"), Delay2));
+	GetWorldTimerManager().SetTimer(MemberTimerHandle, this, &ABo_Mechanic::BallAttack, Delay2,false);
 
-	if (temp == 0) {
-		Spawn(MySceneThird);
-	}
-	else if (temp == 1) {
-		Spawn(MySceneSecond);
-		Spawn(MySceneForth);
-	}
-	else if (temp == 2) {
-		Spawn(MySceneFirst);
-		Spawn(MySceneThird);
-		Spawn(MySceneFifth);
-	}
-	else if (temp == 3) {
-		Spawn(MySceneFirst);
-		Spawn(MySceneSecond);
-		Spawn(MySceneForth);
-		Spawn(MySceneFifth);
-	}
-	else if (temp == 4) {
-		Spawn(MySceneFirst);
-		Spawn(MySceneSecond);
-		Spawn(MySceneThird);
-		Spawn(MySceneForth);
-		Spawn(MySceneFifth);
-	}
+
+
+
 
 }
+
+void ABo_Mechanic::BallAttack()
+{
+	GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Yellow, FString::Printf(TEXT("Yo!!!")));
+
+	if (Delay2 == 2) {
+		Spawn(MySceneThird);
+	}
+	else if (Delay2 == 3) {
+		Spawn(MySceneSecond);
+		Spawn(MySceneForth);
+	}
+	else if (Delay2 == 4) {
+		Spawn(MySceneFirst);
+		Spawn(MySceneThird);
+		Spawn(MySceneFifth);
+	}
+	else if (Delay2 == 5) {
+		Spawn(MySceneFirst);
+		Spawn(MySceneSecond);
+		Spawn(MySceneForth);
+		Spawn(MySceneFifth);
+	}
+	else if (Delay2 == 6) {
+		Spawn(MySceneFirst);
+		Spawn(MySceneSecond);
+		Spawn(MySceneThird);
+		Spawn(MySceneForth);
+		Spawn(MySceneFifth);
+	}
+
+	GetRadomDelay();
+}
+
+
 
 
 
